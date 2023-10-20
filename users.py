0e121699296679ff0b4417e93f04be1c9d462e79
Bilbaobilbo <54086908+Bilbaobilbo@users.noreply.github.com>
2023-10-20 02:53:32 -0300

    add files users and SDW2023

---
+++ users.py
@@ -0,0 +1,54 @@
+{
+    "id": 4225,
+    "name": "user_test_1",
+    "account": {
+      "id": 4478,
+      "number": "02201-1",
+      "agency": "001",
+      "balance": 0,
+      "limit": 500
+    },
+    "card": {
+      "id": 4088,
+      "number": "**** *22* **** 1111",
+      "limit": 1000
+    },
+    "features": [],
+    "news": []
+  },
+  {
+    "id": 4226,
+    "name": "user_test_2",
+    "account": {
+      "id": 4479,
+      "number": "02301-2",
+      "agency": "001",
+      "balance": 0,
+      "limit": 500
+    },
+    "card": {
+      "id": 4089,
+      "number": "**** *23* **** 2222",
+      "limit": 1000
+    },
+    "features": [],
+    "news": []
+  },
+  {
+    "id": 4227,
+    "name": "user_test_3",
+    "account": {
+      "id": 4480,
+      "number": "02401-3",
+      "agency": "001",
+      "balance": 0,
+      "limit": 500
+    },
+    "card": {
+      "id": 4090,
+      "number": "**** *24* **** 3333",
+      "limit": 1000
+    },
+    "features": [],
+    "news": []
+  },
